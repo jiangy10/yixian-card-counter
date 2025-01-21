3408cc028734e7363b9bbfa4c37ee38c3024b18b
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2025-01-20 23:16:31 -0500

    prepare for api

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class main : using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    
+}
+
+public class Player
+{
+    public string id;
+    public string username;
+    public int Destiny;
+    public int Health;
+    public int Cultivation;
+    
+}
