a463d907f841777028f76dae7a57549bc0170f34
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2025-02-03 23:37:55 -0500

    fixed label spacing ?

--- Assets/Scripts/StyleManager.cs
+++ Assets/Scripts/StyleManager.cs
@@ -24,20 +24,37 @@ public static class StyleManager
             Debug.LogError("PlayerInfoContainer not found.");
             return;
         }
+
+
         var userNameLabel = root.Q<Label>("UserNameLabel");
         if (userNameLabel != null)
         {
             userNameLabel.style.fontSize = playerInfoContainer.resolvedStyle.width * 0.06f;
+            if(playerInfoContainer.resolvedStyle.width <= 70){
+                userNameLabel.style.paddingTop = 0;
+                userNameLabel.style.paddingBottom = 0;
+            }
+
         }
+
         var cultivationLabel = root.Q<Label>("CultivationLabel");
         if (cultivationLabel != null)
         {
             cultivationLabel.style.fontSize = playerInfoContainer.resolvedStyle.width * 0.05f;
+            if(playerInfoContainer.resolvedStyle.width <= 70){
+                cultivationLabel.style.paddingTop = 0;
+                cultivationLabel.style.paddingBottom = 0;
+            }
         }
+
         var healthLabel = root.Q<Label>("HealthLabel");
         if (healthLabel != null)
         {
             healthLabel.style.fontSize = playerInfoContainer.resolvedStyle.width * 0.05f;
+            if(playerInfoContainer.resolvedStyle.width <= 70){
+                healthLabel.style.paddingTop = 0;
+                healthLabel.style.paddingBottom = 0;
+            }
         }
     }
 }
