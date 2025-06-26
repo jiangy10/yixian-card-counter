0b8f856d32f00d9c962aea5f57d5490f32458a85
rhit-jiangy10 <jiangy10@rose-hulman.edu>
2025-06-25 23:49:42 -0700

    refactored PlayerSection TrackingCard and MatchHistory to MatchRecord

---
+++ src/components/MatchRecord.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PlayerSelector from './PlayerSelector';
+import PlayerInfoContainer from './PlayerInfoContainer';
+import TrackingCardContainer from './TrackingCardContainer';
+import MatchHistoryContainer from './MatchHistoryContainer';
+import { Player } from '../models/model';
+import { Card } from '../models/model';
+
+interface MatchRecordProps {
+  players: Player[];
+  selectedPlayer: Player;
+  displayCards: Card[];
+  trackingHeight: number;
+  onPlayerSelect: (player: Player) => void;
+  onMouseDown: (e: React.MouseEvent) => void;
+}
+
+const MatchRecord: React.FC<MatchRecordProps> = ({
+  players,
+  selectedPlayer,
+  displayCards,
+  trackingHeight,
+  onPlayerSelect,
+  onMouseDown,
+}) => {
+  return (
+    <>
+      <PlayerSelector 
+        players={players} 
+        onPlayerSelect={onPlayerSelect} 
+      />
+      <PlayerInfoContainer player={selectedPlayer} />
+      <div className="container-wrapper">
+        <div className="tracking-container" style={{ height: trackingHeight }}>
+          <TrackingCardContainer cards={displayCards} />
+        </div>
+        <div 
+          className="resizer"
+          onMouseDown={onMouseDown}
+        />
+        <div className="history-container">
+          <MatchHistoryContainer matchHistory={selectedPlayer?.match_history} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MatchRecord;
