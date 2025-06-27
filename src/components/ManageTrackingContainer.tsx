e46c318f26a8dcc70a46246c97767fc87ae2ed73
rhit-jiangy10 <jiangy10@rose-hulman.edu>
2025-06-26 18:04:25 -0700

    push for release

--- src/components/ManageTrackingContainer.tsx
+++ src/components/ManageTrackingContainer.tsx
@@ -8,11 +8,11 @@ interface ManageTrackingContainerProps {
 const ManageTrackingContainer: React.FC<ManageTrackingContainerProps> = ({ onManageClick }) => {
   return (
     <div className="manage-tracking-container">
+      {/* <button className="manage-tracking-button" onClick={onManageClick}>
+        牌库
+      </button> */}
       <button className="manage-tracking-button" onClick={onManageClick}>
         管理追踪卡牌
-      </button>
-      <button className="manage-tracking-button" onClick={onManageClick}>
-        牌库
       </button>
     </div>
   );
