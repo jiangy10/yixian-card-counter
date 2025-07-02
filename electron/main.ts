4f2ff9497ededde62433eb033bd73d45e510e576
Yuxuan <jiangy10@rose-hulman.edu>
2025-07-02 15:34:36 -0700

    fixed npm make issue

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import * as path from 'path';
 import * as fs from 'fs/promises';
-import * as isDev from 'electron-is-dev';
+import isDev from 'electron-is-dev';
 import './battleLogConverter';
 import './cardOperationLogConverter';
 
