bd1de482363d5112c039972b2d849194747c5d26
yuxuanj <yuxuanj@adobe.com>
2026-05-31 16:13:54 -0700

    updated card changing logic

--- electron/cardOperationLogConverter.ts
+++ electron/cardOperationLogConverter.ts
@@ -69,13 +69,13 @@ function calculateDeckCards(operations: CardOperation[]): { [key: string]: CardC
       }
     }
     
-    if (op.operation === 1) { // replace card: srcCard -2 from deck, dstCard -1 from deck
+    if (op.operation === 1) { // replace card: srcCard -3 from deck, dstCard -1 from deck
       if (op.dstCard.name && !op.dstCard.name.includes('梦•')) { // SEASONAL EFFECT
         if (op.srcCard.name) {
           if (!cardCounts[op.srcCard.name]) {
             cardCounts[op.srcCard.name] = { count: 0 };
           }
-          cardCounts[op.srcCard.name].count += 2;
+          cardCounts[op.srcCard.name].count += 3;
         }
         if (op.dstCard.name) {
           if (!cardCounts[op.dstCard.name]) {
